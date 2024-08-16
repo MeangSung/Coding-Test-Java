04540929c73f3dd858d6d3646d4aba8d32dd8cc0
MeangSung <aoddlsgh98@gmail.com>
2024-08-16 13:31:39 +0900

    merge

--- Coding-Test/src/Main.java
+++ Coding-Test/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
+    private static final List<Set<Integer>> direction = Arrays.asList(
+            new HashSet<>(Arrays.asList(1, 0)),
+            new HashSet<>(Arrays.asList(0, 1)),
+            new HashSet<>(Arrays.asList(-1, 0)),
+            new HashSet<>(Arrays.asList(0, -1))
+    );
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] r = {6,1,2,3,4,5};
+        Arrays.sort(r);
+        System.out.println(r[r.length-1]);
     }
 }
