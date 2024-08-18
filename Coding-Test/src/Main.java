30be0dde7e70406ad698f29f01eb05ca3c6d0026
MeangSung <aoddlsgh98@gmail.com>
2024-08-18 13:39:29 +0900

    [programers] Lv3 여행경로

--- Coding-Test/src/Main.java
+++ Coding-Test/src/Main.java
@@ -1,12 +1,11 @@
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.Set;
 import java.util.StringTokenizer;
 
@@ -21,10 +20,19 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(),"+-/*",true);
 
-        System.out.println(st.nextToken());
-        System.out.println(st.nextToken());
-        System.out.println(st.nextToken());
-        System.out.println(st.nextToken());
+        String[][] r = {{"ICN", "SFO"},{"ICN", "ATL"},{"SFO", "ATL"},{"ATL", "ICN"},{"ATL","SFO"}};
+        Arrays.sort(r,new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                return !o1[0].equals(o2[0])?o1[0].compareTo(o2[0]):o1[1].compareTo(o2[1]);
+            }
+        });
+        for(String[] rr : r){
+            System.out.println(Arrays.toString(rr));
+
+        }
+
+        List<String[]> ss = new ArrayList<>();
 
     }
 }
