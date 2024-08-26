076bb417d09a99cc21c6e2702a30fcf11a9686d7
MeangSung <aoddlsgh98@gmail.com>
2024-08-26 21:00:47 +0900

    [baekjoon] S1_1497

--- Coding-Test/src/Main.java
+++ Coding-Test/src/Main.java
@@ -1,9 +1,4 @@
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String a= "1";
-        a+="c";
-        System.out.println(a.charAt(0));
+    public static void main(String[] args)  {
     }
-}
+    }
