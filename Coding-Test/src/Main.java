e85730db565b9ec92dca072ffdcef2fcf7568b75
MeangSung <aoddlsgh98@gmail.com>
2024-08-28 15:40:33 +0900

    [programers] Lv2 모음사전

--- Coding-Test/src/Main.java
+++ Coding-Test/src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args)  {
         String a = "skf";
-        String aa = Integer.toBinaryString(a.length());
-        System.out.println(aa);
 
     }
     }
