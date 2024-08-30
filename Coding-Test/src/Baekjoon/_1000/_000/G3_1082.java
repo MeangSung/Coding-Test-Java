39787721bb2c2f814d0dea1301d970f25579dc01
MeangSung <aoddlsgh98@gmail.com>
2024-08-30 19:20:20 +0900

    [baekjoon] G3_1082

--- Coding-Test/src/Baekjoon/_1000/_000/G3_1082.java
+++ Coding-Test/src/Baekjoon/_1000/_000/G3_1082.java
@@ -1,7 +1,11 @@
-import java.io.*;
-import java.util.*;
+package Baekjoon._1000._000;
 
-public class Main{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class G3_1082 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
