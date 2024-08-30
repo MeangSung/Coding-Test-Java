ef19d7d3f1f9c442a5d7b7a8d5309955433f4552
MeangSung <aoddlsgh98@gmail.com>
2024-08-30 12:55:09 +0900

    [baekjoon] G3_1005

--- Coding-Test/src/Main.java
+++ Coding-Test/src/Main.java
@@ -1,47 +1,14 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main{
-    private static int n;
-    private static int[] top;
-    private static int[] res;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        map.put(1, new ArrayList<>());
+        map.get(1).add(2);
 
-        int n = Integer.parseInt(br.readLine());
-
-        top = new int[n+1];
-        res = new int[n];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 1; i < n+1; i++){
-            top[i] = Integer.parseInt(st.nextToken());
-        }
-
-        sol();
-
-        // for(int r : res){
-        //     System.out.printf(r+" ");
-        // }
-    }
-
-    private static void sol(){
-
-        for(int i = 2; i < n+1; i++){
-            int cnt = 0;
-            for(int j = i; j > 1; j--){
-                System.out.println(cnt);
-
-                if(top[i-1] >= top[i]){
-                    cnt = i;
-                    break;
-                }
-            }
-            System.out.println(cnt);
-            res[i-1] = cnt;
-        }
+        System.out.println(map.keySet());
     }
 }
