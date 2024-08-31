f2da9883a1ba4dafb244263ece37ff013946b3dd
MeangSung <aoddlsgh98@gmail.com>
2024-08-31 19:43:14 +0900

    [baekjoon] G3_1099

--- Coding-Test/src/Baekjoon/_1000/_000/G3_1099.java
+++ Coding-Test/src/Baekjoon/_1000/_000/G3_1099.java
@@ -1,74 +1,77 @@
-import java.io.*;
-import java.util.*;
+package Baekjoon._1000._000;
 
-public class Main{
-    public static void main(String[] args){
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class G3_1099 {
+    public static void main(String[] args) {
         FastScan fs = new FastScan();
 
         String hStr = fs.nextLine();
         int n = fs.nextInt();
         String[] words = new String[n];
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++) {
             words[i] = fs.nextLine();
         }
 
         System.out.println(sol(hStr,n,words));
     }
 
-    private static int sol(String hStr, int n, String[] words){
+    private static int sol(String hStr, int n, String[] words) {
         int[] dp = new int[hStr.length()+1];
-        Arrays.fill(dp,Integer.MAX_VALUE-51);
-        dp[0] = 0;
-
+        Arrays.fill(dp,Integer.MAX_VALUE - 51);
         String[] splitHStr;
-        for(int i = 1; i < hStr.length()+1; i++){
+
+        dp[0] = 0;
+        for(int i = 1; i <= hStr.length(); i++) {
             splitHStr = new String[i];
-            for(int j = 0; j < i; j++){
+            for(int j = 0; j < i; j++) {
                 splitHStr[j] = hStr.substring(j,i);
             }
 
-            for(int j = 0; j < splitHStr.length; j++){
+            for(int j = 0; j < splitHStr.length; j++) {
                 for(int k = 0; k < n; k++){
                     if(!compareWords(splitHStr[j], words[k])) continue;
 
-                    int diff = diff(splitHStr[j], words[k]);
+                    int diff = diff(splitHStr[j],words[k]);
 
-                    if(j==0) {
-                        dp[i] = Math.min(dp[i],diff);
+                    if(j == 0 ) {
+                        dp[i] = Math.min(dp[i], diff);
                         continue;
-                    }
+                    };
 
-                    dp[i] = Math.min(dp[i], dp[j]+diff);
+                    dp[i] = Math.min(dp[i],diff+dp[j]);
                 }
             }
         }
-
-        if(dp[hStr.length()] != Integer.MAX_VALUE - 51){
+        if(dp[hStr.length()] != Integer.MAX_VALUE - 51) {
             return dp[hStr.length()];
-        }
-        else{
+        }else{
             return -1;
         }
     }
 
-    private static int diff(String w1, String w2){
+    private static int diff(String w1, String w2) {
         int cnt = 0;
-        for(int i = 0; i < w1.length(); i++){
-            if(w1.charAt(i) == w2.charAt(i)) cnt++;
+        for(int i = 0; i < w1.length(); i++) {
+            if(w1.charAt(i) != w2.charAt(i)) cnt++;
         }
         return cnt;
     }
 
-    private static boolean compareWords(String w1, String w2){
-        if (w1.length() != w2.length()) return false;
+    private static boolean compareWords(String w1, String w2) {
+        if(w1.length() != w2.length()) return false;
         int[] check = new int[26];
-        for(int i = 0; i < w1.length(); i++){
+        for(int i = 0; i < w1.length(); i++) {
             check[w1.charAt(i) - 'a']++;
             check[w2.charAt(i) - 'a']--;
         }
-        for(int c : check){
-            if(c != 0) return false;
+        for(int i = 0; i < 26; i++) {
+            if(check[i] != 0) return false;
         }
         return true;
     }
@@ -86,20 +89,19 @@ public class Main{
                 try{
                     st = new StringTokenizer(br.readLine());
                 }
-                catch(IOException e){
+                catch (IOException e){
                     e.printStackTrace();
                 }
             }
             return st.nextToken();
         }
 
-        int nextInt(){return Integer.parseInt(next());}
+        int nextInt(){ return Integer.parseInt(next()); }
         String nextLine(){
             String str = "";
             try{
                 str = br.readLine();
-            }
-            catch(IOException e){
+            }catch (IOException e){
                 e.printStackTrace();
             }
             return str;
