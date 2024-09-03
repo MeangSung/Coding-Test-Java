4143cfc7c6c273f6528f63fc14a46d375fac5727
MeangSung <aoddlsgh98@gmail.com>
2024-09-03 09:45:23 +0900

    [baekjoon] G2_1135

--- Coding-Test/src/Baekjoon/_1000/_100/G2_1135.java
+++ Coding-Test/src/Baekjoon/_1000/_100/G2_1135.java
@@ -1,10 +1,20 @@
-import java.util.*;
-import java.io.*;
+package Baekjoon._1000._100;
 
-public class Main{
-    private static List<Integer>[] tree;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class G2_1135 {
     private static int[] dp;
-    public static void main(String[] args){
+    private static List<Integer>[] tree;
+
+    public static void main(String[] args) {
         FastScan fs = new FastScan();
 
         int n = fs.nextInt();
@@ -14,12 +24,13 @@ public class Main{
         int[] input = Arrays.stream(fs.nextLine().split(" "))
                 .mapToInt(Integer::parseInt).toArray();
         for(int i = 1; i < n; i++) tree[input[i]].add(i);
+
         System.out.println(dfs(0));
     }
 
     private static int dfs(int cur){
         int cnt = 0, max = 0;
-        Queue<int[]> q = new PriorityQueue<>();
+        Queue<int[]> q = new PriorityQueue<>((o1,o2) -> o2[1] - o1[1]);
         for(Integer next : tree[cur]){
             dp[next] = dfs(next);
             q.add(new int[]{next, dp[next]});
@@ -28,9 +39,8 @@ public class Main{
         while(!q.isEmpty()){
             int[] node = q.poll();
             cnt++;
-            max = Math.max(max,cnt+node[1]);
+            max = Math.max(max, node[1]+cnt);
         }
-
         return max;
     }
 
@@ -47,24 +57,24 @@ public class Main{
                 try{
                     st = new StringTokenizer(br.readLine());
                 }
-                catch(IOException e){
+                catch (IOException e){
                     e.printStackTrace();
                 }
             }
             return st.nextToken();
         }
 
-        int nextInt(){return Integer.parseInt(next());}
+        int nextInt(){ return Integer.parseInt(next()); }
+
         String nextLine(){
             String str = "";
             try{
                 str = br.readLine();
             }
-            catch(IOException e){
+            catch (IOException e){
                 e.printStackTrace();
             }
             return str;
         }
     }
-
-}
+  }
