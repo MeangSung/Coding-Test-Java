51a5a62c5a98b368770851a77adebf20d86c34d6
MeangSung <aoddlsgh98@gmail.com>
2024-09-05 15:17:52 +0900

    commit

---
+++ Coding-Test/src/Baekjoon/_1000/_200/G4_1229.java
@@ -0,0 +1,14 @@
+package Baekjoon._1000._200;
+
+import java.util.Scanner;
+
+public class G4_1229 {
+    static int n, sizN, res;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+    }
+
+
+}
