d8b9211cdb6deae7dbe2c8fe7196cfceea6e8c4c
MeangSung <aoddlsgh98@gmail.com>
2024-09-22 15:51:54 +0900

    [baekjoon] S3_1051

--- Coding-Test/src/Baekjoon/_1000/_000/S3_1051.java
+++ Coding-Test/src/Baekjoon/_1000/_000/S3_1051.java
@@ -1,8 +1,13 @@
-import java.io.*;
-import java.util.*;
+package Baekjoon._1000._000;
 
-public class Main{
-    public static void main(String[] args){
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S3_1051 {
+    public static void main(String[] args) {
         FastScan fs = new FastScan();
 
         int n = fs.nextInt();
@@ -11,35 +16,35 @@ public class Main{
         int[][] board = new int[n][m];
 
         for(int i = 0; i < n; i++){
-            String arr = fs.nextLine();
+            String str = fs.nextLine();
             for(int j = 0; j < m; j++){
-                board[i][j] = arr.charAt(j) - '0';
+                 board[i][j] = str.charAt(j) - '0';
             }
         }
 
-        System.out.println(sol(n,m,board));
+        System.out.println((int) Math.pow(sol(n,m,board),2));
     }
 
     private static int sol(int n, int m, int[][] board){
-
         int max = 1;
 
-        int len = Math.min(n,m);
+        int len = Math.min(n, m);
 
         while(len > 1){
-            for(int i = 0; i <= n-len; i++){
-                for(int j = 0; j <= m-len; j++){
+            for(int i = 0; i <= n - len;i++){
+                for(int j = 0; j <= m - len;j++){
                     int num = board[i][j];
                     if(num == board[i][j+len-1] && num == board[i+len-1][j] && num == board[i+len-1][j+len-1]){
-                        max = Math.max(max,len);
+                        max = Math.max(max, len);
                     }
                 }
             }
             len--;
         }
 
-        return max*max;
+        return max;
     }
+
 
     static class FastScan {
         BufferedReader br;
