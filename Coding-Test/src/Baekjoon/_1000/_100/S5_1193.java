8e758d8b344810d92e0d6d4bad58f2094b299fd4
MeangSung <aoddlsgh98@gmail.com>
2024-09-26 13:59:14 +0900

    [baekjoon] S5_1193

--- Coding-Test/src/Baekjoon/_1000/_100/S5_1193.java
+++ Coding-Test/src/Baekjoon/_1000/_100/S5_1193.java
@@ -1,7 +1,10 @@
+package Baekjoon._1000._100;
+
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
+public class S5_1193 {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
@@ -9,13 +12,13 @@ public class Main{
         int squareCount = 1, squareSum = 0;
 
         while(true){
-            if(n <= squareCount+squareSum){
+            if(n <= squareCount + squareSum){
                 if(squareCount % 2 == 1){
-                    System.out.println((squareCount - (n - squareSum - 1))+ "/" + (n-squareSum));
+                    System.out.println(squareCount - (n - squareSum -1) + "/" + (n - squareSum));
                     break;
                 }
                 else{
-                    System.out.println((n-squareSum) + "/" + (squareCount - (n - squareSum - 1)));
+                    System.out.println(n-squareSum + "/" + (squareCount - (n - squareSum -1) ));
                     break;
                 }
             }
