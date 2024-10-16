62ecd0a1215b8e978fb889aa8929006d31b1f85c
MeangSung <aoddlsgh98@gmail.com>
2024-10-16 10:39:10 +0900

    [baekjoon] G1_1035

--- Coding-Test/src/Main.java
+++ Coding-Test/src/Main.java
@@ -1,7 +1,93 @@
-class Main {
-    public static void main(String[] args) {
-        char[] c = {'a', 'b', 'c'};
+import java.io.*;
+import java.util.*;
 
-        System.out.println((char) ('A' + 1));
+public class Main{
+    private int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+    private ArrayList<Pos> pieces;
+    private boolean[][] loc = new boolean[5][5];
+    private int n;
+    private int res = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException{
+        new Main().sol();
+    }
+
+    private void sol() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        pieces = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++){
+            String strArr = br.readLine();
+            for(int j = 0; j < 5; j++){
+                if(strArr.charAt(j) == '*'){
+                    pieces.add(new Pos(i,j));
+                }
+            }
+        }
+
+        n = pieces.size();
+        bfs(0,0);
+        System.out.println(res);
+    }
+
+    private void bfs(int idx, int sum){
+        if(res <= sum) return;
+        if(idx == n){
+            if(check()){
+                res = sum;
+            }
+            return;
+        }
+
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                if(loc[i][j]) continue;
+                int distant = Math.abs(pieces.get(idx).x - i) + Math.abs(pieces.get(idx).y - j);
+                loc[i][j] = true;
+                bfs(idx+1, sum + distant);
+                loc[i][j] = false;
+            }
+        }
+    }
+
+    private boolean check(){
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                if(loc[i][j]){
+                    Stack<Pos> stk = new Stack<>();
+                    boolean[][] validate = new boolean[5][5];
+                    int cnt = 0;
+                    stk.push(new Pos(i,j));
+                    validate[i][j] = true;
+                    while(!stk.isEmpty()){
+                        Pos cur = stk.pop();
+                        cnt++;
+                        for(int k = 0; k < 4; k++){
+                            int cx = cur.x - directions[k][0];
+                            int cy = cur.y - directions[k][1];
+
+                            if(cx < 0 || cx > 4 || cy < 0 || cy > 4) continue;
+                            if(validate[cx][cy]) continue;
+                            if(!loc[cx][cy]) continue;
+
+                            validate[cx][cy] = true;
+                            stk.push(new Pos(cx,cy));
+                        }
+                    }
+
+                    return n == cnt;
+                }
+            }
+        }
+        return false;
+    }
+
+    class Pos{
+        int x,y;
+
+        public Pos(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
     }
 }
