69db1b5a495dd82c08fd494fdf6beb4ecab03cc7
MeangSung <aoddlsgh98@gmail.com>
2024-11-29 11:13:15 +0900

    [programers] Lv.2 충돌위험찾기

--- Coding-Test/src/Main.java
+++ Coding-Test/src/Main.java
@@ -1,14 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(func(6,100,5));
+        int[][] points = {{3, 2}, {6, 4},{4, 7},{1, 4}};
+        int[][] routes = {{4, 2},{1, 3},{4, 2},{4, 3}};
 
+        System.out.println(new Solution().solution(points,routes));
 
-    }
-
-    private static  long func(long a, long b, long c){
-        long val =1 ;
-        while(b-- >= 0) val = val * a % c;
-        return val;
     }
 }
