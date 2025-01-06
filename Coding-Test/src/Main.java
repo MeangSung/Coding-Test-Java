0bccdd4c57eb351dc211229a147af27d1c68bdf9
MeangSung <aoddlsgh98@gmail.com>
2025-01-06 15:37:54 +0900

    [programers] Lv3 입국심사

--- Coding-Test/src/Main.java
+++ Coding-Test/src/Main.java
@@ -1,10 +1,10 @@
+import Programers.Lv3.입국심사;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[][] points = {{3, 2}, {6, 4},{4, 7},{1, 4}};
-        int[][] routes = {{4, 2},{1, 3},{4, 2},{4, 3}};
+        입국심사 test = new 입국심사();
 
-        System.out.println(new Solution().solution(points,routes));
-
+        System.out.println(test.solution(6,new int[]{7,10}));
     }
 }
