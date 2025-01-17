b7f2af65a18335a371b3d2f2689286d1c732b7a5
MeangSung <aoddlsgh98@gmail.com>
2025-01-17 14:06:27 +0900

    [baekjoon] S4_1822

--- Coding-Test/src/Baekjoon/_00000/_1000/_800/S4_1822.java
+++ Coding-Test/src/Baekjoon/_00000/_1000/_800/S4_1822.java
@@ -1,11 +1,18 @@
-import java.util.*;
-import java.io.*;
+package Baekjoon._00000._1000._800;
 
-public class Main{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class S4_1822 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
@@ -13,19 +20,18 @@ public class Main{
         int[] bArr = new int[m];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++) {
             aArr[i] = Integer.parseInt(st.nextToken());
         }
-
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < m; i++){
+        for(int i = 0; i < m; i++) {
             bArr[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(sol(n,m,aArr,bArr));
+        System.out.println(sol(aArr,bArr));
     }
 
-    private static String sol(int n, int m, int[] aArr, int[] bArr){
+    private static String sol(int[] aArr, int[] bArr) {
         StringBuilder sb = new StringBuilder();
 
         Arrays.sort(aArr);
@@ -34,12 +40,12 @@ public class Main{
         List<Integer> list = new ArrayList<>();
 
         for(int num : aArr){
-            if(Arrays.binarySearch(bArr,num) < 0) list.add(num);
+            if(Arrays.binarySearch(bArr, num) < 0) list.add(num);
         }
 
         sb.append(list.size()).append("\n");
-        for (Integer integer : list) {
-            sb.append(integer).append(" ");
+        for (int num : list) {
+            sb.append(num).append(" ");
         }
 
         return sb.toString();
