e1e9feaba4641ed167d85854e2aa1536f4cde8ec
MeangSung <aoddlsgh98@gmail.com>
2025-02-21 12:25:43 +0900

    [baekjoon] G3_9466

--- Coding-Test/src/Baekjoon/_00000/_9000/_400/G3_9466.java
+++ Coding-Test/src/Baekjoon/_00000/_9000/_400/G3_9466.java
@@ -1,32 +1,33 @@
+package Baekjoon._00000._9000._400;
+
+import java.io.IOException;
 import java.util.Scanner;
 
-public class Main{
+public class G3_9466 {
     static final int NOT_VISITED = 0;
     static final int CYCLE_IN = -1;
-    static int[] arr, state;
-    public static void main(String[] args){
+    static int[] state, arr;
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
-
         int t = sc.nextInt();
 
-        while(t-- > 0){
+        while(t-- > 0) {
             int n = sc.nextInt();
+            arr = new int[n + 1];
+            state = new int[n + 1];
 
-            arr = new int[n+1];
-            state = new int[n+1];
-
-            for(int i = 1; i <= n; i++){
+            for(int i = 1; i <= n; i++) {
                 arr[i] = sc.nextInt();
             }
 
-            for(int i = 1; i <= n; i++){
+            for(int i = 1; i <= n; i++) {
                 if(state[i] == NOT_VISITED) sol(i);
             }
 
             int cnt = 0;
-            for(int i = 1; i <= n; i++){
-                if(state[i] != CYCLE_IN) cnt++;
+            for(int i = 1; i <= n; i++ ){
+                if(state[i] != CYCLE_IN ) cnt++;
             }
 
             sb.append(cnt).append("\n");
@@ -35,13 +36,13 @@ public class Main{
         System.out.println(sb);
     }
 
-    private static void sol(int start){
-        int cur = start;
+    private static void sol(int x) {
+        int cur = x;
         while(true){
-            state[cur] = start;
+            state[cur] = x;
             cur = arr[cur];
 
-            if(state[cur] == start){
+            if(state[cur] == x){
                 while(state[cur] != CYCLE_IN){
                     state[cur] = CYCLE_IN;
                     cur = arr[cur];
@@ -49,7 +50,7 @@ public class Main{
                 return;
             }
 
-            if(state[cur] != NOT_VISITED) return;
+            else if(state[cur] != NOT_VISITED) return;
         }
     }
 
